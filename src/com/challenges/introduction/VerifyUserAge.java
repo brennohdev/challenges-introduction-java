d9a34d43517620d3e156a69b67fc658b13e94c22
Brenno Henrique <brennohdev@gmail.com>
2025-05-19 23:31:23 -0300

    VerifyUserAge Challenge

---
+++ src/com/challenges/introduction/VerifyUserAge.java
@@ -0,0 +1,29 @@
+package com.challenges.introduction;
+
+import java.util.Scanner;
+
+public class VerifyUserAge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter how many people you want to check: ");
+        int quantityOfPeople = scanner.nextInt();
+
+        int legalAgeCount = 0;
+
+        for (int i = 0; i < quantityOfPeople; i++) {
+            System.out.println("Enter the person " + (i + 1) + ": ");
+            int userAge = scanner.nextInt();
+
+            boolean isUserAge = userAge >= 18;
+
+            if (isUserAge) {
+                legalAgeCount++;
+            }
+
+        }
+
+        System.out.println("Number of people of legal age: " + legalAgeCount);
+        scanner.close();
+    }
+}
